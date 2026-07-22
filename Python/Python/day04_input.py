6e44eb14aebbf88aedc96468b0affa656c6af26b
Afrin <afrinnahrin406@gmail.com>
2026-07-22 19:26:16 +0600

    Delete Python/Python directory

--- Python/Python/day04_input.py
+++
@@ -1,8 +0,0 @@
-name = input("Name: ")
-department = input("Department: ")
-semester = input("Semester: ")
-
-print("----- Student Information -----")
-print("Name:", name)
-print("Department:", department)
-print("Semester:", semester)
